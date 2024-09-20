41118c0068a3924f236687c839aae7666210cb42
pcori-santander_sangroup <pcori@externos.com.pe>
2024-09-20 17:35:51 -0500

    c

--- src/main/java/com/login/entity/ResponseValidateUser.java
+++ src/main/java/com/login/entity/ResponseValidateUser.java
@@ -74,7 +74,7 @@ public class ResponseValidateUser {
         this.role = role;
     }
 
-    public boolean isFirstLogin() {
+    public boolean getIsFirstLogin() {
         return isFirstLogin;
     }
 
