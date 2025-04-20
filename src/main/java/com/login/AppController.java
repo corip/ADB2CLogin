4964d161bffdfe9fd56cd0cea5c5cb253930a342
pedro cori amesquita <coripedro3@gmail.com>
2025-04-20 14:13:11 -0500

    cambios

--- src/main/java/com/login/AppController.java
+++ src/main/java/com/login/AppController.java
@@ -1,14 +1,16 @@
 package com.login;
 
+import com.login.entity.Request;
+import com.login.entity.Response;
 import com.login.entity.ResponseError;
 import com.login.entity.ResponseValidateUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/")
@@ -21,5 +23,27 @@ public class AppController {
         response.setIsValidUser(true);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("login")
+    private ResponseEntity login(@RequestBody Request request){
+        Response response = new Response();
+        ResponseError responseError = new ResponseError();
+        Random random = new Random();
+        int promoCode= random.nextInt(1000) + 1;
+
+        logger.debug("/login");
+        if(!request.getEmail().equalsIgnoreCase("pepe@gmail.com")){
+            responseError.setVersion("1.0.1");
+            responseError.setStatus(409);
+            responseError.setUserMessage("LoyaltyId ID '123' is not associated with 'pepe@gmail.com' email address.");
+
+            return new ResponseEntity<>(responseError, HttpStatus.CONFLICT);
+        }
+
+        response.setPromoCode(""+promoCode);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
     }
 }
